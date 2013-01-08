cde120631ad07b1af1dc76f2f13d7f92a9ce0423
lsjustin <lsjustin89@gmail.com>
2013-01-08 21:27:29 +0800

    admin, publickey

---
+++ server/certification-center/admin.py
@@ -0,0 +1,23 @@
+import webapp2
+
+from google.appengine.ext import db
+
+class AdminKeys(db.Model):
+    pubkey = db.TextProperty(default="pubkey")
+    privkey= db.TextProperty(default="privkey")
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type']='text/plain'
+        keys=AdminKeys.all()
+        k=keys.get()
+        if not k:
+            new_keys=AdminKeys(pubkey="public key",private="privkey")
+            new_keys.put()
+        elif k.privkey=="privkey":
+            k.privkey="private key"
+            k.pubkey ="public key"
+            k.put()
+        k=keys.get()
+        self.response.write(k.privkey)
+app=webapp2.WSGIApplication([('/admin',MainPage)],
+                            debug=True)
---
+++ server/certification-center/publickey.py
@@ -0,0 +1,17 @@
+import webapp2
+
+from google.appengine.ext import db
+
+class AdminKeys(db.Model):
+    pubkey = db.TextProperty(default="pubkey")
+    privkey= db.TextProperty(default="privkey")
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type']='text/plain'
+        keys=AdminKeys.all()
+        k=keys.get()
+        self.response.write(k.pubkey)
+
+app=webapp2.WSGIApplication([('/publickey',MainPage)],
+                            debug=True)
